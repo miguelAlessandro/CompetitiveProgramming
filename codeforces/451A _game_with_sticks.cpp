59688cbd08d2b39f282c573a1ab445b0c8a1dd4b
miguelAlessandro <mminih@uni.pe>
2016-02-18 20:53:13 -0500

    Update 451A _game_with_sticks.cpp

--- codeforces/451A _game_with_sticks.cpp
+++ codeforces/451A _game_with_sticks.cpp
@@ -7,8 +7,7 @@ int main(){
     
     int n, m;
     scanf("%d %d", &n, &m);
-    if(min(n, m)&1) puts("Akshat");
-    else puts("Malvika");
+    puts(min(n, m)&1 ? "Akshat" : "Malvika");
 
     return 0;
 }
