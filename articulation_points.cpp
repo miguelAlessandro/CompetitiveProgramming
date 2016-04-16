b1734212a6345c8b082ef70299a64340dd8b75eb
miguel <mminih@uni.pe>
2016-04-16 04:20:36 -0500

    hola

--- articulation_points.cpp
+++ articulation_points.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
-#include <cstdio>
+#include <utility>
+#define N 100005
 
 using namespace::std;
 
-void dfs_complete(int n){
-    memset(visit, -1, sizeof vis)
+int vis[N], T, low[N];
+vector<int> adj[N];
+vector<int> cut_point;
 
+
+void dfs_complete(int n){
+
+    memset(visit, -1, sizeof vis);
+    cut_point.clear();
+    T = 0;
+    for(int i = 0; i < n; ++i) 
+        if(not vis[i])
+            dfs(i)
 }
 
+void dfs(int x, int P){
+
+    vis[x] = T;
+    low[x] = T;
+    T = T + 1;
+    int children = 0;
+    for(int i = 0; i < int(adj[x].size()); ++i){
+        int u = adj[x][i];
+        if(u == P) continue;
+        if(vis[u] == -1){
+            dfs(u, x);
+            if(low[u] >= vis[x] and p != -1)
+                cut_point.push_back(x);
+            low[x] = min(low[x], low[u]);
+            ++children;
+        }
+        else low[x] = min(low[x], vis[u]);
+    }
+    if(p == -1 and children > 1)
+        cut_point.push_back(x);
+}
 
 int main(){
-
 
     return 0;
 }
