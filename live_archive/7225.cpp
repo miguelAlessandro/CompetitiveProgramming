609488b409658fb8ef6074ac8e9df195ea8e1125
Miguel Mini <mminih@uni.pe>
2016-05-27 01:31:08 +0000

    7225.cpp 0.016s

--- live_archive/7225.cpp
+++ live_archive/7225.cpp
@@ -2,29 +2,28 @@
 
 using namespace::std;
 
-int t, n, m, x, ant;
-int minimom;
-vector<int> V[55];
-long long sum;
+int t, n, m, nc=1;
+long long sum, f, l, x;
 
-long long mcd(long long a, long long b){return b == 0 ? a : mcd(b, a%b);}
+long long mcd(long long a, long long b){
+	return b ? mcd(b, a%b) : abs(a);
+}
 
 int main(){
     scanf("%d", &t);
-    for(int k = 1; k <= t; ++k){
-        scanf("%d", &n);
-		sum = 0;
+    while(t--){
+        scanf("%d", &n); l = sum = 0;;
         for(int i = 0; i < n; ++i){
-            scanf("%d", &m); V[i].resize(m);
+            scanf("%d", &m);  
 			for(int j = 0; j < m; ++j){
-				scanf("%d", x);
-            	if(i+j==0) ant = x;
-				else ant = abs(mcd(ant, x));
-				if(i==0) sum += x;
+				scanf("%lld", &x);
+				if(j==0)
+					 sum += x, f = x;
+				else
+					l = mcd(l, x-f);
 			}
-		}
-    
-        printf("Case #%d: %lld\n", k, mcd(ant, sum));
+		} 
+        printf("Case #%d: %lld\n", nc++, mcd(l, sum));
     }
 
     return 0;
