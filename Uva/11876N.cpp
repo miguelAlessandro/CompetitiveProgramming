b8518237ee6a09eb64d10b876fcfbfb29a943c14
miguelAlessandro <mminih@uni.pe>
2016-08-27 04:35:55 -0500

    Update and rename 11876 - N + NOD (N) to 11876N.cpp

--- Uva/11876N.cpp
+++ Uva/11876N.cpp
@@ -1,9 +1,21 @@
-#include <stdio.h>
-#define N 1000002
+#include <cstdio>
+const int N = 1000002;
 
 int p[N], solve[N];
 int n, a, b, term = 1, nc = 1, nd, f, c, i, j;
 short v[N];
+
+#define getcx getchar_unlocked
+inline void inp( int &n )//fast input function
+{
+   n=0;
+   int ch=getcx();int sign=1;
+   while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getcx();}
+ 
+   while(  ch >= '0' && ch <= '9' )
+           n = (n<<3)+(n<<1) + ch-'0', ch=getcx();
+   n=n*sign;
+}
 
 void criba(){
     for(i = 4; i < N; i += 2) p[i] = 2;
@@ -41,9 +53,9 @@ int main(){
     for(i = 1; i < N; ++i)
         solve[i] = solve[i-1] + v[i];       
 
-    scanf("%d", &n);
+    inp(n);
     while(n--){
-        scanf("%d %d", &a, &b);
+        inp(a); inp(b);
         printf("Case %d: %d\n", nc++, solve[b] - solve[a-1]);
     }
 
