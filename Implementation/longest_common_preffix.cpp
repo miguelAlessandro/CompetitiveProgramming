7e3b1e9cbfb1c388bdd933b6be70f6b90e4b18a4
miguelAlessandro <mminih@uni.pe>
2017-02-08 16:32:08 -0500

    :v

---
+++ Implementation/longest_common_preffix.cpp
@@ -0,0 +1,17 @@
+int lcp(int x, int y){ 
+    if (x == y) return n-x;
+    int r = 0;
+  
+    int len = 0;
+    while((1<<len) < 2*n) len++;
+  
+    for (int i = len-1; i >= 0; --i)
+        if(x >= n or y >= n) break;
+        else if(P[i][x] == P[i][y]){
+            x += (1<<i);
+            y += (1<<i);
+            ret += (1<<i); 
+        }
+      
+    return ret;
+} 
