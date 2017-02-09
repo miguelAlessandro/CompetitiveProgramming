80ad42742866e913467a57e63f845134ec51a64a
miguelAlessandro <mminih@uni.pe>
2017-02-09 00:20:06 -0500

    Update suffix_array.cpp

--- Implementation/suffix_array.cpp
+++ Implementation/suffix_array.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-const int N = (1<<20)+1;
+const int N = 1000005;
 const int LN = 20;
 int P[LN][N], n;
 string A;
@@ -9,7 +9,7 @@ string A;
 struct Point{
   int x, y, p;
   bool operator<(const Point& p){return x < p.x or x == p.x and y < p.y;}
-  bool operator==(const Point& p){return x == p.x and y == p.y;}
+  bool operator!=(const Point& p){return x != p.x or y != p.y;}
 } L[N];
 
 int main(){
@@ -20,7 +20,7 @@ int main(){
     for(int i = 0; i < n; ++i) 
         P[0][i] = A[i] - 'a';
         
-    for(int i = 0; (1<<i) < 2*n; ++i){
+    for(int i = 0; (1<<i) < (n<<1); ++i){
         for (int j = 0; i < n; ++j){
             L[j].x = P[i-1][j];
             L[j].y = j+(1<<i) < n ? P[i-1][j+(1<<i)] : -1;
@@ -28,8 +28,10 @@ int main(){
         }
         sort(L, L+n);      
         P[i][L[0].p] = 0;
-        for(int j = 1; j < n; ++j) 
-            P[i][L[j].p] = L[j] == L[j-1] ? P[i][L[j-1].p] : j;
+        for(int cr = 0, j = 1; j < n; ++j){
+            if(L[j] != L[j-1]) cr++; 
+            P[i][L[j].p] = cr;
+        }
      } 
       
      return 0;
