8c36702f7ba1537e9b44e262d662ec08b1253837
peon-pasado <xam_is_threat@outlook.com>
2019-09-21 23:15:55 -0500

    uva

--- UVA/10807.cpp
+++ UVA/10807.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define all_elements_in(vect) begin(vect), end(vect)
 using namespace std; 
 const int maxN = 12;
 const int maxM = 30;
@@ -7,19 +8,33 @@ struct Edge {
 	int a, b, w;
 };
 int n, m;
+
 int p[maxN], rnk[maxN];
+
+int get(int x) {
+	return x == p[x] ? x : p[x] = get(p[x]);
+}
+
 int components;
 
-int get(int x) {return x == p[x] ? x : p[x] = get(p[x]);}
 bool merge(int x, int y) {
-	x = get(x);
-	y = get(y);
-	if (x == y) return 0;
-	if (rnk[x] < rnk[y]) swap(x, y);
+	x = get(x); y = get(y);
+	if (x == y) {
+		return 0;
+	}
+	if (rnk[x] < rnk[y]) {
+		swap(x, y);
+	}
 	rnk[x] += rnk[x] == rnk[y];
 	p[y] = x;
 	components -= 1;
 	return 1;
+}
+
+void make_tree(int x) {
+	components += 1;
+	p[x] = x;
+	rnk[x] = 0;
 }
 
 int main() {
@@ -31,35 +46,62 @@ int main() {
 			cin >> a >> b >> w;
 			edges.push_back({a, b, w});
 		}
-		if (m < 2*(n-1)) {
-			cout << "No way!" << endl;
-			continue;
-		}
-		sort(begin(edges), end(edges), [](Edge p, Edge q) {
+		sort(all_elements_in(edges), [](Edge p, Edge q) {
 			return p.w < q.w;
 		});
-		for (int i = 1; i <= n; ++i) p[i] = i, rnk[i] = 0;
-		bool arr[m]; memset(arr, 0, sizeof arr);
-		int ans = 0;
-		components = n;
-		for (int i = 0; i < m; ++i) {		
+		for (int i = 1; i <= n; ++i) {
+			make_tree(i);		
+		}
+		vector<int> in_mst;
+		for (int i = 0; i < edges.size(); ++i) {
 			if (merge(edges[i].a, edges[i].b)) {
-				ans += edges[i].w;
-				arr[i] = 1;
+				in_mst.push_back(i);
 			}
 		}
-		bool fun = 1;
-		if (components != 1) fun = 0;
-		components = n;
-		for (int i = 1; i <= n; ++i) p[i] = i, rnk[i] = 0;
-		for (int i = 0; i < m; ++i) {		
-			if (arr[i]) continue;
-			if (merge(edges[i].a, edges[i].b)) {
-				ans += edges[i].w;
+		vector<bool> block_edges(m);
+		int t = in_mst.size();
+		int ans = 1e9;
+		for (int bit_set = 0; bit_set < 1<<t; bit_set += 2) {
+			fill(all_elements_in(block_edges), 0);
+			for (int i = 0; i < t; ++i) {
+				if (bit_set & (1<<i)) {
+					block_edges[in_mst[i]] = 1;
+				}
+			}
+			int mst1 = 0;
+			components = 0;
+			for (int i = 1; i <= n; ++i) {
+				make_tree(i);
+			}
+			for (int i = 0; i < edges.size(); ++i) {
+				if (not block_edges[i] and merge(edges[i].a, edges[i].b)) {
+					mst1 += edges[i].w;
+					block_edges[i] = 1;
+				}
+			}
+			if (components != 1) {
+				continue;
+			}
+			for (int i = 0; i < t; ++i) {			
+				if (bit_set & (1<<i)) {
+					block_edges[in_mst[i]] = 0;
+				}
+			}
+			int mst2 = 0;
+			components = 0;
+			for (int i = 1; i <= n; ++i) {
+				make_tree(i);
+			}	
+			for (int i = 0; i < edges.size(); ++i) {
+				if (not block_edges[i] and merge(edges[i].a, edges[i].b)) {
+					mst2 += edges[i].w;
+				}
+			}
+			if (components == 1) {
+				ans = min(ans, mst1 + mst2);
 			}
 		}
-		if (components != 1) fun = 0;
-		if (not fun) cout << "No way!" << endl;
+		if (ans > 1e6) cout << "No way!" << endl;
 		else cout << ans << endl;
 	}
 	return 0;
